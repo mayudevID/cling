bc137b155911e4a5343579931062bfa89a6aeaa0
maulana2468 <ymaulana089@gmail.com>
2023-07-25 17:58:01 +0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,19 @@ import 'package:cling/core/route.dart';
 
 import 'package:cling/features/main/statistics/bloc/statistics_bloc.dart';
 import 'package:cling/resources/gen/fonts.gen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
