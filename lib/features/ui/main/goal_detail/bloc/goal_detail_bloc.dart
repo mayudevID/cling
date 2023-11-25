b5e2e797e832e80a9fb0782ccd1f541f3eedbd2c
maulana2468 <ymaulana089@gmail.com>
2023-11-25 21:40:25 +0700

    update podfile

--- lib/features/ui/main/goal_detail/bloc/goal_detail_bloc.dart
+++ lib/features/ui/main/goal_detail/bloc/goal_detail_bloc.dart
@@ -19,8 +19,6 @@ class GoalDetailBloc extends Bloc<GoalDetailEvent, GoalDetailState> {
   void _initGoal(InitGoal event, emit) async {
     final result = await _dbRepo.getGoalDetailSave(event.goalModel.id!);
 
-    print(result);
-
     emit(state.copyWith(
       goalModel: event.goalModel,
       dataSavingsList: result,
